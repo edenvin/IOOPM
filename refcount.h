17f4a9cd0e17b6d7c8659a301d128f4782b2c695
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2012-10-22 10:44:41 +0200

    Added refcount.h

---
+++ refcount.h
@@ -0,0 +1,10 @@
+#ifndef __refcount_h
+#define __refcount_h
+
+/*
+ * refcount.h
+ *
+ * This file contains private specifications for using the imalloc
+ * allocator used as a course project for the 2012 IOOP/M course.
+ *
+ */
