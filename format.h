e3ce6c08c942f205a9e194bfc91ed87318eaf5a6
Daniel <daniel.lindgren@density.se>
2012-10-22 11:07:38 +0200

    Lade till format.h

---
+++ format.h
@@ -0,0 +1,16 @@
+#ifndef __imalloc_h
+#define __imalloc_h
+/*
+ * imalloc.h
+ *
+ * This file contains the public specifications for using the
+ * imalloc allocator used as a course project for the 2012 IOOP/M
+ * course.
+ *
+ * Note that this specification might evolve as the project is
+ * running, f.ex. due to technical as well as educational bugs and
+ * insights.
+ *
+ * You may NOT alter the definitions in this file.
+ *
+ */
