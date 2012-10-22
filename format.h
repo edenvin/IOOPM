e2b90e512dc485dbc15d8365cd963a4ccba40820
Daniel <daniel.lindgren@density.se>
2012-10-22 14:05:41 +0200

    Uppdaterade format.h, enligt Guds önskan

--- format.h
+++ format.h
@@ -3,16 +3,17 @@
 /*
  * format.h
  *
- * This file contains the public specifications for the formatstring
+ * This file contains the public specifications for the format string
  * used as a course project for the 2012 IOOP/M course.
  *
  */
 #include <imalloc.h>
-typedef char* formatstring;
+
+typedef char* format_string;
 
 /* 
- *Returns the size needed equivalent to the flag, e.g. flag d returns the size of an int
+ * Returns the size needed equivalent to the parse_format, e.g. parse_format d returns the size of an int
  */
-chunk_size flag_to_size (char *flag);
+chunk_size format_flag_to_size (format_string parse_format);
 
  #endif
