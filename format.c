5249566c3060ad0c9a89311605cde10a9477c71f
bailer <thebailern1@gmail.com>
2012-10-23 09:44:24 +0200

    Added format.c

---
+++ format.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include "format.h"
+
+chuck_size format_string_to_size(format_string str) {
+  int i = 0;
+  while (str[i] != '\0') {
+    switch (str[i]) {
+      case '*'
+    }
+  }
