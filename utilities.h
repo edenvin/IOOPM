dabdc7a4a3ad537c87c4cc15a638ca19db3b1920
bailer <thebailern1@gmail.com>
2012-10-23 13:06:46 +0200

    Added macron for navigating to freelist, alloclist and refcount and back

--- utilities.h
+++ utilities.h
@@ -1,5 +1,17 @@
 #ifndef __utilities_h
 #define __utilities_h
+#define REFCOUNT(p) ((unsigned int*) (p) - 1)
+#define OBJECT(p) ((void*) ((p) + 1))
+#define IMALLOC_TO_FREE ((Chunk) (p) - 2)
+#define IMALLOC_TO_ALLOC ((Chunk) (p) - 1)
+#define FREE_TO_ALLOC ((p) + 1)
+#define FREE_TO_IMALLOC ((style) ((p) + 2))
+#define ALLOC_TO_FREE ((p) - 1)
+#define ALLOC_TO_IMALLOC ((style) ((p) + 1))
+
+#include "imalloc.h"
+#include "memory.h"
+
 /*
  * utilities.h
  *
