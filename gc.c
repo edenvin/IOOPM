ce09340e62e81cf1df010520afe51e328165cc30
Daniel <daniel.lindgren@density.se>
2012-10-24 11:50:24 +0200

    Update with mainly some comments added

--- gc.c
+++ gc.c
@@ -1,6 +1,5 @@
 #include "gc.h"
 
-
 /*
  * Typedefs from the specification
  */
@@ -8,17 +7,27 @@ typedef char *RawPtr;
 typedef struct {
 RawPtr start;
 RawPtr end;
-} addressspace;
+} address_space;
 
 typedef void (*MarkFun)(void *ptr, void *data);
 
 traverse_stack (adress_space h, mark_fun f, void *p);
-
+/*
+ * Funciton to mark all the chunk's (in alloclist) that were found in the stack as alive.
+ * In other words, set the mark bit to true.
+ */
 void ptr_mark(void *ptr, void *ignore, style mem) {
   set_memory_status(memory_search(*ptr, mem), true);
 }
 
+/*
+ * Our functions
+ */
 
+ /*
+  * The first stage of the mark and sweep algorithm.
+  * Traverses the alloclist and sets all mark_bits to false.
+  */
 void stage_one (style mem){
   while (!next_chunk){           // be om next_chunk
     set_memory_status (chunk,false);
@@ -33,9 +42,9 @@ void stage_one (style mem){
  */
 unsigned int collect (style mem){
   stage_one (mem);
-  *addressspace as;
-  as->start = //första objektet i adressrymden
-  as->end = //sista objektet i adressrymden
+  address_space as;
+  as->start = //first object in the heap's addresspace
+  as->end = //last object in the heap's addresspace
   traverse_stack (&as, ptr_mark (*ptr, *ignore, mem), NULL);
-  
+
 }
