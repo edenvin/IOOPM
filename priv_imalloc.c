352e16d15781efeba46200dc02742526449ae2d4
Åke Lagercrantz <ake@norbyit.se>
2012-10-25 06:05:54 -0400

    Fixed compiler errors.

--- priv_imalloc.c
+++ priv_imalloc.c
@@ -1,6 +1,42 @@
 #include "priv_imalloc.h"
 
 
+/* Initiates the malloc library to be used. memsiz defines the
+ * maximum amount of memory that can be used. flags specifies kind
+ * of memory manager and allows fine-tunes some options.
+ */
+struct style *priv_imalloc(chunk_size memsiz, unsigned int flags) { return NULL; }
+
+
+/* 
+ * Frees an object. 
+ */
+void ifree(void *object) {}
+
+/* 
+ * Allocates memory for the given chunk size 
+ * and returns a pointer for the allocated memory 
+ */
+void* ialloc(chunk_size size) { return NULL; }
+
+/*
+ * Allocates memory for the given chunk size 
+ * and returns a pointer for the allocated memory 
+ */
+void* manual_alloc(chunk_size size) { return NULL; }
+
+/*
+ * Allocates memory for the given chunk size 
+ * and returns a pointer for the allocated memory 
+ */
+void* managed_alloc(chunk_size size) { return NULL; }
+
+/*
+ * Allocates memory for the given chunk size as a string
+ * and returns a pointer for the allocated memory 
+ */
+void* typed_alloc(format_string size) { return NULL; }
+
 /*
  * Converts a priv_mem pointer to a style pointer.
  */
