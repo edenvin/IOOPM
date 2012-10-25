3972d54b2cb4047afec8a5feedc72eb27813ecef
unknown <andreas@lelli.se>
2012-10-25 14:28:21 +0200

    Minor fixes

--- gc.c
+++ gc.c
@@ -11,12 +11,12 @@ typedef struct {
 
 typedef void (*MarkFun)(void *ptr, address_space h, priv_mem mem);
 
-
+/* Scans the stack, CPU registers, and static data to build a
+* root set, R s.t. for all r in R, h->start <= r && r <= h->end.
+* Then, for all r in R, calls f(r, p), where p is just some
+* additional user-provided payload.
+*/
 void traverse_stack (address_space h, mark_fun f, void *p);
-/*
- * Our functions
- */
-
 
 /*
  * Returns true if pointer ptr is within the addresspace on the heap that was allocated using iMalloc
@@ -36,7 +36,6 @@ Boolean in_address_space(void *ptr, address_space h) {
  * Objects on the heap must be "connected" to the stack via
  * one (or more) pointers to be considered alive.
  */
-
 void traverse_heap(void *ptr, address_space h, priv_mem mem){
   chunk_size size = 0;
   Chunk chunk = alloclist (mem);
@@ -56,14 +55,13 @@ void traverse_heap(void *ptr, address_space h, priv_mem mem){
   return NULL;
 }
 
-
  /*
   * The first stage of the mark & sweep algorithm.
   * Traverses the alloclist and sets all mark_bits to false.
   */
-void mark_unused(priv_mem mem) {
+void mark_unused(priv_mem mem){
   Chunk current_chunk = alloclist(mem);
-  while (current_chunk) {           // be om next_chunk
+  while (current_chunk){
     set_memory_status(current_chunk ,false);
     current_chunk = next_chunk(current_chunk);       
   } 
@@ -74,19 +72,18 @@ void mark_unused(priv_mem mem) {
  * Frees the chunks that is no longer being used, the ones with
  * the mark bit set to false.
  */
-int sweep(priv_mem mem) {
+int sweep(priv_mem mem){
   int i = 0;
   Chunk current_chunk = alloclist(mem); 
   //Initiate iterator
-  while (current_chunk) {
-    if (memory_is_marked(current_chunk) == FALSE) {
+  while (current_chunk){
+    if (memory_is_marked(current_chunk) == FALSE){
       free_memory(current_chunk);
       i++;
       current_chunk = next_chunk(current_chunk);
     }
-    else {
+    else
       current_chunk = next_chunk(current_chunk);
-    }
   }
   return i;
 }
@@ -96,7 +93,7 @@ int sweep(priv_mem mem) {
  * Returns a positive integer if the sweep stage was successful and memory was freed.
  * The integer returned is corresponding to the number of memory blocks freed.
  */
-unsigned int collect(Memory mem) {
+unsigned int collect(Memory mem){
   priv_mem memory_private = style_to_priv(mem);
   mark_unused(memory_private);
   address_space as;
