f9d89c97d36c6f8f85b5c882d2d791e1120edca0
Åke Lagercrantz <ake@norbyit.se>
2012-10-29 10:47:38 -0400

    Lifted setting of the managed function pointers in priv_imalloc to own function.

--- priv_imalloc.c
+++ priv_imalloc.c
@@ -25,23 +25,15 @@ struct style *priv_imalloc(chunk_size memsiz, unsigned int flags) {
     new_mem->functions.managed.alloc = &managed_alloc;
     // MANAGED + REFCOUNT
     if (flags < 21) {
-      new_mem->functions.managed.rc.retain  = &retain;
-      new_mem->functions.managed.rc.release = &release;
-      new_mem->functions.managed.rc.count   = &count;
-      new_mem->functions.managed.gc.alloc   = NULL;
-      new_mem->functions.managed.gc.collect = NULL;
+      set_priv_mem_managed_functions(new_mem, &retain, &release, &count, NULL, NULL);
       new_mem->lists = lists_based_on_flags(flags, 17, 18, 20, new_mem->start, memsiz);
   // MANAGED + GC
     } else if (flags < 37) {
-      new_mem->functions.managed.rc.retain  = NULL;
-      new_mem->functions.managed.rc.release = NULL;
-      new_mem->functions.managed.rc.count   = NULL;
-      new_mem->functions.managed.gc.alloc   = &typed_alloc;
-      new_mem->functions.managed.gc.collect = &collect;
+      set_priv_mem_managed_functions(new_mem, NULL, NULL, NULL, &typed_alloc, &collect);
       new_mem->lists = lists_based_on_flags(flags, 33, 34, 36, new_mem->start, memsiz);
   // MANAGED + REFCOUNT + GC
     } else {
-      set_priv_mem_functions((Priv_mem) new_mem, &retain, &release, &count, &typed_alloc, &collect);
+      set_priv_mem_managed_functions(new_mem, &retain, &release, &count, &typed_alloc, &collect);
       new_mem->lists = lists_based_on_flags(flags, 49, 50, 52, new_mem->start, memsiz);
     }
     return priv_to_style((Priv_mem) new_mem);
@@ -51,7 +43,7 @@ struct style *priv_imalloc(chunk_size memsiz, unsigned int flags) {
 /*
  * Set function pointers to a priv_mem struct.
  */
-void set_priv_mem_functions(Priv_mem mem, unsigned int (*retain)(void *object), unsigned int (*release)(Memory mem, void *object), unsigned int (*count)(void *object), void* (*typed_alloc)(Memory mem, format_string size), unsigned int (*collect)(Memory mem)) {
+void set_priv_mem_managed_functions(Priv_mem mem, unsigned int (*retain)(void *object), unsigned int (*release)(Memory mem, void *object), unsigned int (*count)(void *object), void* (*typed_alloc)(Memory mem, format_string size), unsigned int (*collect)(Memory mem)) {
   mem->functions.managed.rc.retain  = retain;
   mem->functions.managed.rc.release = release;
   mem->functions.managed.rc.count   = count;
--- priv_imalloc.h
+++ priv_imalloc.h
@@ -40,7 +40,7 @@ struct style *priv_imalloc(chunk_size memsiz, unsigned int flags);
 /*
  * Set function pointers to a priv_mem struct.
  */
-void set_priv_mem_functions(Priv_mem mem, unsigned int (*retain)(void *object), unsigned int (*release)(Memory mem, void *object), unsigned int (*count)(void *object), void* (*typed_alloc)(Memory mem, format_string size), unsigned int (*collect)(Memory mem));
+void set_priv_mem_managed_functions(Priv_mem mem, unsigned int (*retain)(void *object), unsigned int (*release)(Memory mem, void *object), unsigned int (*count)(void *object), void* (*typed_alloc)(Memory mem, format_string size), unsigned int (*collect)(Memory mem));
 
 /*
  * Returns a Lists object based on flags and x, y, z.
