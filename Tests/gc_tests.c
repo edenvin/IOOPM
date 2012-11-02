8b0d197206c4765b99fe5c9bf6a0b939fadc2c39
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2012-11-02 08:42:52 +0100

    Modified gc_tests

--- Tests/gc_tests.c
+++ Tests/gc_tests.c
@@ -94,7 +94,10 @@ int gc_tests(int (*init_suite)(void), int (*clean_suite)(void)) {
   
   // Add tests
   if (
-    (NULL == CU_add_test(gc_suite, "test of collect()", test_collect_1))
+    (NULL == CU_add_test(gc_suite, "test of collect()", test_collect_1)) ||
+    (NULL == CU_add_test(gc_suite, "test of collect()", test_collect_2)) ||
+    (NULL == CU_add_test(gc_suite, "test of collect()", test_collect_3)) ||
+    (NULL == CU_add_test(gc_suite, "test of collect()", test_collect_4))
   ) {
     CU_cleanup_registry();
     return CU_get_error();
